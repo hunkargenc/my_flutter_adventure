6672781e3775743628f2d5ad7c0f6ee851a91b23
Hünkar Genç Yıldız <hunkar.genc11@hacettepe.edu.tr>
2021-09-12 18:50:45 +0300

    theme practise
    theme practise

--- my_project/lib/main.dart
+++ my_project/lib/main.dart
@@ -1,113 +1,168 @@
+import 'package:dynamic_themes/dynamic_themes.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+/// The app's themes.
+/// This class is just there to connect readable names
+/// to integer theme IDs.
+class AppThemes {
+  static const int LightBlue = 0;
+  static const int LightRed = 1;
+  static const int DarkBlue = 2;
+  static const int DarkRed = 3;
+
+  static String toStr(int themeId) {
+    switch (themeId) {
+      case LightBlue:
+        return "Light Blue";
+      case LightRed:
+        return "Light Red";
+      case DarkBlue:
+        return "Dark Blue";
+      case DarkRed:
+        return "Dark Red";
+      default:
+        return "Unknown";
+    }
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final dark = ThemeData.dark();
+    final darkButtonTheme =
+        dark.buttonTheme.copyWith(buttonColor: Colors.grey[700]);
+    final darkFABTheme = dark.floatingActionButtonTheme;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+    final themeCollection = ThemeCollection(themes: {
+      AppThemes.LightBlue: ThemeData(primarySwatch: Colors.blue),
+      AppThemes.LightRed: ThemeData(primarySwatch: Colors.red),
+      AppThemes.DarkBlue: dark.copyWith(
+          accentColor: Colors.blue,
+          buttonTheme: darkButtonTheme,
+          floatingActionButtonTheme:
+              darkFABTheme.copyWith(backgroundColor: Colors.blue)),
+      AppThemes.DarkRed: ThemeData.from(
+          colorScheme:
+              ColorScheme.dark(primary: Colors.red, secondary: Colors.red)),
+    });
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+    return DynamicTheme(
+        themeCollection: themeCollection,
+        defaultThemeId: AppThemes.LightBlue,
+        builder: (context, theme) {
+          return MaterialApp(
+            title: 'dynamic_themes example',
+            theme: theme,
+            home: HomePage(title: 'dynamic_themes example app'),
+          );
+        });
+  }
+}
 
+class HomePage extends StatefulWidget {
   final String title;
 
+  const HomePage({Key? key, required this.title}) : super(key: key);
+
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _HomePageState extends State<HomePage> {
+  int dropdownValue = 0;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final theme = Theme.of(context);
+    dropdownValue = DynamicTheme.of(context)!.themeId;
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+          child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 24, bottom: 12),
+            child: Text('Select your theme here:'),
+          ),
+          DropdownButton(
+              icon: Icon(Icons.arrow_downward),
+              value: dropdownValue,
+              items: [
+                DropdownMenuItem(
+                  value: AppThemes.LightBlue,
+                  child: Text(AppThemes.toStr(AppThemes.LightBlue)),
+                ),
+                DropdownMenuItem(
+                  value: AppThemes.LightRed,
+                  child: Text(AppThemes.toStr(AppThemes.LightRed)),
+                ),
+                DropdownMenuItem(
+                  value: AppThemes.DarkBlue,
+                  child: Text(AppThemes.toStr(AppThemes.DarkBlue)),
+                ),
+                DropdownMenuItem(
+                  value: AppThemes.DarkRed,
+                  child: Text(AppThemes.toStr(AppThemes.DarkRed)),
+                )
+              ],
+              onChanged: (dynamic themeId) async {
+                await DynamicTheme.of(context)!.setTheme(themeId);
+                setState(() {
+                  dropdownValue = themeId;
+                });
+              }),
+          Container(
+              margin: EdgeInsets.all(20),
+              width: 100,
+              height: 100,
+              color: theme.primaryColor,
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                      'Container in primary color and primary text theme',
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).primaryTextTheme.bodyText2),
+                ),
+              )),
+          Container(
+            margin: EdgeInsets.all(20),
+            width: 100,
+            height: 100,
+            color: theme.accentColor,
+            child: Center(
+                child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                  'Container in accent color and with accent text theme',
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).accentTextTheme.bodyText2),
+            )),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Elevated Button'),
+          ),
+        ],
+      )),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.notifications), label: "Notifications"),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: () {},
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
